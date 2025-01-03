b325d77cdee9924787d112bbf89d8729f3430a2e
Balexo <oscar.vallejo@outlook.com>
2025-01-03 11:42:28 +0100

    Exercise24 solved

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,26 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> cola = new LinkedList<>();
+
+        cola.offer("Smith");
+        cola.offer("Montessori");
+        cola.offer("Peralta");
+        cola.offer("House");
+
+        return cola;
 
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (queue.size()>0){
+            String element = queue.poll();
+            System.out.println("Elemento " + element);
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> cola = createQueue();
+        printAndEmptyQueue(cola);
     }
 
 }
